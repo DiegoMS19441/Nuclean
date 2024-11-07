a72b941a2268cbb9fc1ca17ce3c9136cd256b2ee
Diego.M <127765045+DiegoMS19441@users.noreply.github.com>
2024-11-07 20:44:14 -0300

    iniciando o projeto no git

---
+++ src/main/java/br/com/fiap/Nuclean/NucleanApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.Nuclean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NucleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NucleanApplication.class, args);
+	}
+
+}
